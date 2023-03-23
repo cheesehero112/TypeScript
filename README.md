065f47242964d215c2ef05949560ad53edeafe47
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-03-22 18:11:21 -0700

    read me updated

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ This is a repo where I organize my learning process of TypeScript
 
 ## Repo Structure
 
+- features
+  - annotations
+    - functions.ts
+    - variables.ts
+  - arrays.ts
+  - classes.ts
+  - interfaces.ts
+  - objects.ts
+  - tuples.ts
+  - types.ts
 - fetch json : simple example of fetching json data using axios and how to avoid typo using TypeScript's interface and providing type to function arguments
 
 - README.md : this file
--- features/classes.ts
+++ features/classes.ts
@@ -23,8 +23,111 @@ class Car extends Vehicle {
 
 const vehicle = new Vehicle();
 // I cannot call honk since it's marked as protected
-vehicle.honk();
+// vehicle.honk();
 
 const car = new Car();
 car.startDrivingProcess();
-car.honk();
+// car.honk();
+
+let index1: number = 1;
+
+function turingTest() {
+  let index2: number = 2;
+
+  if (index2 > index1) {
+    let index3: number = 3;
+    index3++;
+  }
+
+  while (index1 < index2) {
+    let index4: number = 4;
+    index1++;
+  }
+
+  console.log(index1);
+  console.log(index2);
+  // console.log(index3);
+  // console.log(index4);
+}
+
+// turingTest();
+
+// function Foo() {
+//   this.bar = 10;
+// }
+// Foo.prototype.bar = 42;
+
+// var foo = new Foo();
+// console.log(foo.bar);
+
+// delete foo.bar;
+// console.log(foo.bar);
+
+// b: any = [];
+// let a = [] + b + 'turing'.split('');
+// console.log(a);
+
+// var languages = ['1', '2', '3'];
+// languages.length = 0;
+// languages.push('4');
+// console.log(languages);
+
+// var p = new Promise((r, rej) => {
+//   rej(Error('message'));
+// });
+
+// p.catch((error: any) => console.log(error.message));
+// p.catch((error: any) => console.log(error.message));
+
+// const a = [1, 2];
+// a[0] = 10;
+// a[1] = 20;
+// a[2] = 30;
+// console.log(a);
+
+// let arr = [1, 'turing', { x: 2 }, [3, 4]];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 'turing') arr.splice(i, 1);
+//   else console.log(arr[i]);
+// }
+
+// let a = [1, 'a', { x: 1 }, [1, 2], { x: 1 }];
+// console.log(a.indexOf({ x: 1 }));
+
+// const x = [1, 2, 3];
+// x[-1] = -1;
+// console.log(x[x.indexOf(10000)]);
+
+// console.log(typeof x);
+// console.log(Number('10') - 10 == 0);
+
+// class TuringPoint {
+//   x: number;
+//   y: number;
+// }
+// interface Point3d extends TuringPoint {
+//   z: number;
+// }
+// let point3d: Point3d = { x: 3, y: 8, z: 9 };
+// console.log(point3d);
+
+// module Vehicle {
+//   class Car {
+//     constructor(public make: string, public model: string) {}
+//   }
+//   var tesla = new Car('tesla', 's');
+// }
+
+// var honda = new Vehicle.Car('honda', 'crv');
+// console.log(honda);
+
+var index = 1,
+  count = {};
+
+count: {
+  begin: 2;
+  baz: ++index;
+}
+
+count.begin + count.index + index;
